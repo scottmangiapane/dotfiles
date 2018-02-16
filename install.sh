4ddca3541b0aa8ede341a389cf933d025c4b8fec
Scott Mangiapane <scottmangiapane@gmail.com>
2018-02-16 13:31:20 -0500

    Added initial dotfiles

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+ln -sv "~/.dotfiles/bash/bashrc"            ~/.bashrc
+ln -sv "~/.dotfiles/conky/conky.config"     ~/.config/conky/conky.conf
+ln -sv "~/.dotfiles/git/gitconfig"          ~/.gitconfig
+ln -sv "~/.dotfiles/git/gitignore_global"   ~/.gitignore_global
+ln -sv "~/.dotfiles/i3/config"              ~/.config/i3/config
+ln -sv "~/.dotfiles/octave/octaverc"        ~/.octaverc
+ln -sv "~/.dotfiles/ssh/authorized_keys"    ~/.ssh/authorized_keys
+ln -sv "~/.dotfiles/tmux/tmux.conf"         ~/.tmux.conf
+ln -sv "~/.dotfiles/vim/vimrc"              ~/.vimrc
+ln -sv "~/.dotfiles/xmonad/xmonad.hs"       ~/.xmonad/xmonad.hs
+ln -sv "~/.dotfiles/zsh/custom.zsh-theme"   ~/.oh-my-zsh/custom/themes/custom.zsh-theme
+ln -sv "~/.dotfiles/zsh/zshrc"              ~/.zshrc
