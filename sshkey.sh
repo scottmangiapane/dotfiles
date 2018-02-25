8e8f5eaa64dbab9d02a140cee78e0cab2118e2b5
Scott Mangiapane <scottmangiapane@gmail.com>
2018-02-25 18:44:46 -0500

    Updated ssh key generation script

--- sshkey.sh
+++ sshkey.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "This script will generate a new ssh key and add it to ssh agent."
+echo "This script will generate a new SSH key and add it to ssh-agent."
 echo "Enter your email and press [ENTER]: "
 read email
-echo "Creating ssh key for $email..."
+echo "Creating SSH key for $email..."
 
 ssh-keygen -t rsa -b 4096 -C "$email"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
 echo "Done."
+echo "Here is your new SSH key:"
+cat ~/.ssh/id_rsa.pub
