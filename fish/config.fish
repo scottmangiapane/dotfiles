47cc9a27fab75cb019201a1adcb305cf868e94c1
Scott Mangiapane <smangiapane@godaddy.com>
2019-12-04 11:21:15 -0700

    Change python alias; new uuid alias

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,10 @@ set -x PAGER less
 set -x PATH $PATH ~/.bin
 set -x TERM xterm-256color
 
+alias pip pip3
+alias python python3
+alias python2 /usr/bin/python
 alias trizen "trizen --noedit --noinfo"
+alias uuid 'uuidgen | tr "[:upper:]" "[:lower:]"'
 
 source .profile
