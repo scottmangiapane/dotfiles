a61c40bbe3cd5c6bc48ce7d625ef14ad3929c5e3
Scott Mangiapane <smangiapane@godaddy.com>
2019-12-31 12:30:08 -0700

    Updated config.fish python setup for catalina

--- fish/config.fish
+++ fish/config.fish
@@ -4,12 +4,10 @@ set -x EDITOR vim
 set -x LESS "-g -i -M -R -S -w -z-4"
 set -x PAGER less
 set -x PATH $PATH ~/.bin
+set -x PATH $PATH ~/Library/Python/3.7/bin
 set -x TERM xterm-256color
 
-alias pip pip3
-alias python python3
-alias python2 /usr/bin/python
 alias trizen "trizen --noedit --noinfo"
 alias uuid 'uuidgen | tr "[:upper:]" "[:lower:]"'
 
-source .profile
+source .config.fish
