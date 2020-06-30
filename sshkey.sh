b080a7b0f1fe6834a8f68ef15b130af88a60cb3b
Scott Mangiapane <scottmangiapane@gmail.com>
2020-06-29 22:51:40 -0700

    Switched SSH key algorithms; replaced RPI with gemini

--- sshkey.sh
+++ sshkey.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo "This script will generate a new SSH key and add it to ssh-agent."
-echo "Enter your email and press [ENTER]: "
-read email
-echo "Creating SSH key for $email..."
+echo "Creating a new ed25519 SSH key..."
 
-ssh-keygen -t rsa -b 4096 -C "$email"
+ssh-keygen -t ed25519 -C ''
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
 
 echo "Done."
 echo "You can copy the public key to a remote server like this:"
-echo "ssh-copy-id -i ~/.ssh/id_rsa user@host"
+echo "ssh-copy-id -i ~/.ssh/id_ed25519 user@host"
