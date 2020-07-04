9521de3e2dea109d6fbeb68c3c5b0093f1230a78
Scott Mangiapane <scottmangiapane@gmail.com>
2020-07-04 11:56:43 -0700

    Added DOCKER_CONTENT_TRUST to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,13 @@
+alias uuid 'uuidgen | tr "[:upper:]" "[:lower:]"'
+
 set fish_greeting ""
 
+set -x DOCKER_CONTENT_TRUST 1
 set -x EDITOR vim
 set -x LESS "-g -i -M -R -S -w -z-4"
 set -x PAGER less
 set -x PATH $PATH ~/.bin
-set -x PATH $PATH ~/Library/Python/3.7/bin
+set -x PATH $PATH ~/.npm-global/bin
 set -x TERM xterm-256color
-
-alias trizen "trizen --noedit --noinfo"
-alias uuid 'uuidgen | tr "[:upper:]" "[:lower:]"'
 
 source .config.fish
