16dad65fa715b74ffebf41c75edccc994f2cf78e
Scott Mangiapane <scottmangiapane@gmail.com>
2020-07-24 16:05:53 -0700

    Added npmrc for rootless global package installs

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ ln -sfv ~/.dotfiles/fish/fish_prompt.fish  ~/.config/fish/functions/fish_prompt.
 ln -sfv ~/.dotfiles/fish/jcurl.fish        ~/.config/fish/functions/jcurl.fish
 ln -sfv ~/.dotfiles/git/gitconfig          ~/.gitconfig
 ln -sfv ~/.dotfiles/git/gitignore_global   ~/.gitignore_global
+ln -sfv ~/.dotfiles/npm/npmrc              ~/.npmrc
 ln -sfv ~/.dotfiles/ssh/authorized_keys    ~/.ssh/authorized_keys
 ln -sfv ~/.dotfiles/ssh/config             ~/.ssh/config
 ln -sfv ~/.dotfiles/vim/vimrc              ~/.vimrc
