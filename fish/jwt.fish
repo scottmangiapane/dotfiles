49104d52ef84299aeaa9e0c585fcee0b21b82ff4
Scott Mangiapane <scottmangiapane@gmail.com>
2024-08-13 13:13:49 -0700

    Improved JWT decoding

--- fish/jwt.fish
+++ fish/jwt.fish
@@ -1,12 +1,24 @@
 function jwt
-	if test (count $argv) != 1
-		echo "usage: jwt <token>"
-		return 1
-	end
-	if ! type -q jq
-		echo "error: 'jq' is required"
-		return 1
-	end
-	echo $argv[1] | jq -R 'split(".") | .[0],.[1] | @base64d | fromjson'
-	echo "Signature: "(echo $argv[1] | awk -F '.' '{print $3}')
+    if test (count $argv) -ne 1
+        echo "usage: jwt <token>"
+        return 1
+    end
+
+    if not type -q jq
+        echo "error: 'jq' is required"
+        return 1
+    end
+
+    set parts (string split '.' $argv[1])
+
+    if test (count $parts) -ne 3
+        echo "error: Invalid JWT format"
+        return 1
+    end
+
+    echo "Header:"
+    echo $parts[1] | jq -R 'gsub("-";"+") | gsub("_";"/") | @base64d | fromjson'
+    echo "Payload:"
+    echo $parts[2] | jq -R 'gsub("-";"+") | gsub("_";"/") | @base64d | fromjson'
 end
+
