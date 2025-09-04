b790737e3fd1438c5d79148b2eb0e9eace5c5cdd
Scott Mangiapane <scottmangiapane@gmail.com>
2025-09-04 18:30:57 -0500

    Replaced Nothing with Pixel

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,12 @@ set -x TERM xterm-256color
 
 set -x PATH $PATH ~/.bin
 set -x PATH $PATH ~/.cargo/bin
+set -x PATH $PATH ~/.local/bin
 set -x PATH $PATH ~/.npm-global/bin
+set -x PATH $PATH /usr/lib/jvm/default/bin
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 
 source ~/.config.fish
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
